b2a03f52433daa0f1d92810599d8ef859be2f5f5
Eltshan <wu.shen.eltshan@gmail.com>
2014-07-27 09:32:23 -0400

    Added search2 method, return the largest element smaller than target when the target does not appear in given BST

--- AlgrithomPractise/src/commonDataStructures/BST.java
+++ AlgrithomPractise/src/commonDataStructures/BST.java
@@ -6,14 +6,22 @@ package commonDataStructures;
  * @author shen
  * 
  */
-public class BST extends TreeNode {
+public class BST {
 
-	public BST() {
-		
+	TreeNode root;
+
+	public BST(TreeNode T) {
+		root = T;
 	}
 
+	/**
+	 * Search for given target. If target did not exist, return null;
+	 * 
+	 * @param target
+	 * @return
+	 */
 	public TreeNode search(int target) {
-		return searchHelper(root, target);
+		return searchHelper(this.root, target);
 	}
 
 	private TreeNode searchHelper(TreeNode T, int target) {
@@ -26,5 +34,37 @@ public class BST extends TreeNode {
 		else {
 			return searchHelper(T.left, target);
 		}
+	}
+
+	/**
+	 * Search for given taret, if target did not exist, return the largest
+	 * element which is smaller that target.
+	 * 
+	 * 
+	 * @param target
+	 * @return
+	 */
+	public TreeNode search2(int target) {
+		return search2Helper(this.root, target, null);
+	}
+
+	private TreeNode search2Helper(TreeNode T, int target, TreeNode previous) {
+		if (T == null)
+			return previous;// ??
+		if (T.val < target) {
+			return search2Helper(T.right, target, T);
+		}
+		if (T.val > target) {
+			return search2Helper(T.left, target, previous);
+		}
+		return T;
+
+	}
+
+	public static void main(String args[]) {
+		BST root = new BST(TreeNode.generateBinaryTree(new int[] { 1, 2, 3, 4,
+				6, 7, 8 }));
+		System.out.println(root.search2(6).val);
+
 	}
 }
